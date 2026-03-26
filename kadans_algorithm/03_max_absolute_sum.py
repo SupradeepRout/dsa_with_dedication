e6883467a7c415eb75f40ec764e6fb68db44aeb4
Supradeep Rout <supradeeprout2003@gmail.com>
2026-03-26 09:38:23 +0530

    update qn-5

--- kadans_algorithm/03_max_absolute_sum.py
+++ kadans_algorithm/03_max_absolute_sum.py
@@ -1,3 +1,14 @@
+'''
+
+Definition : Given an array of integers, find the maximum absolute sum of a subarray.
+Approach : Kadane's Algorithm
+Time Complexity : O(n) where n is the length of the array
+Space Complexity : O(1)
+Difficulty : Medium
+problem Link : https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/description/
+YouTube Link : https://youtu.be/Bt9rgQgGf64?si=xvgnYPHAft7OP0o_
+
+'''
 
 class max_absolute_sum :
     def maxAbsoluteSum( self , nums ):
@@ -32,7 +43,7 @@ class max_absolute_sum :
     
     
 if __name__ == "__main__":
-    arr=[1,-3,2,3,-4]
+    arr=[1,-3,2,3,-4,5]
     obj = max_absolute_sum()
     result = obj.maxAbsoluteSum(arr)
     print(f"The maximum absolute sum is : {result}")
